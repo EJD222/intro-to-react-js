42e70759a6e3715100d02efc57d4d1e0a5612d09
EL-J Diaz <eljdiaz500@gmail.com>
2023-08-27 15:31:04 +0800

    Push files of Test App

---
+++ test-app/script.js
@@ -0,0 +1,12 @@
+class HelloWorld extends React.Component {
+    constructor (props){
+    super (props);
+    this.state = {personName: " World"};
+}
+    render () {
+        return React.createElement('h1', null, 'Hello,' + this.state.personName);
+    }
+}
+
+const domContainer = document.querySelector("#app");
+ReactDOM.render(React.createElement (HelloWorld), domContainer);
